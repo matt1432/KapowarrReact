5bd1658c8b35a323b0d9f8e3da00bd24e3adbbb4
matt1432 <matt@nelim.org>
2025-05-24 13:21:10 -0400

    chore: update flake.lock

--- src/backend/features/download_queue.py
+++ src/backend/features/download_queue.py
@@ -429,7 +429,6 @@ class DownloadHandler(metaclass=Singleton):
 
         if link_type == "lg" and not isinstance(result, str):
             if "comics_id" in result and result["comics_id"]:
-                LOGGER.info(result["comics_id"])
                 torrent_name = str(int(int(result["comics_id"]) / 1000) * 1000)
                 torrent_link = (
                     f"https://libgen.gs/torrents/comics/c_{torrent_name}.torrent"
