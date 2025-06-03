0df8ba275587a742deca724f8f1fc98e20b135ee
matt1432 <matt@nelim.org>
2025-06-03 19:39:42 -0400

    build: add build step for javascript code

---
+++ setup.py
@@ -0,0 +1,36 @@
+import subprocess
+
+from setuptools import Command, setup  # type: ignore
+from setuptools.command.build import build  # type: ignore
+
+
+def system(command: str) -> type[Command]:
+    class CustomCommand(Command):
+        def initialize_options(self) -> None:
+            pass
+
+        def finalize_options(self) -> None:
+            pass
+
+        def run(self, *args, **kwargs) -> None:
+            subprocess.check_call(command, shell=True)
+
+    return CustomCommand
+
+
+cmds = {"transpile_ts": system("mv typescript src/frontend/static/js")}
+
+
+class BuildCommand(build):
+    # Makes the `cmds` commands sub-commands of the `build_command`, which has the
+    # effect of the formers being invoked when the latter is invoked (which is invoked
+    # in turn when the wheel must be built, through the `bdist_wheel` command)
+    sub_commands = [(key, None) for key in cmds.keys()] + build.sub_commands
+
+
+setup(
+    cmdclass={
+        "build": BuildCommand,
+        **cmds,
+    }
+)
