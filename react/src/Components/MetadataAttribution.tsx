4a468c26bce445e82c9c0fd327014167216e9e67
matt1432 <matt@nelim.org>
2025-07-19 18:24:53 -0400

    fix(react): replace sonarr provider

--- react/src/Components/MetadataAttribution.tsx
+++ react/src/Components/MetadataAttribution.tsx
@@ -3,9 +3,6 @@
 // Misc
 import translate from 'Utilities/String/translate';
 
-// General Components
-import Link from 'Components/Link/Link';
-
 // CSS
 import styles from './MetadataAttribution.module.css';
 
@@ -14,9 +11,7 @@ import styles from './MetadataAttribution.module.css';
 export default function MetadataAttribution() {
     return (
         <div className={styles.container}>
-            <Link className={styles.attribution} to="/settings/metadatasource">
-                {translate('MetadataProvidedBy', { provider: 'TheTVDB' })}
-            </Link>
+            {translate('MetadataProvidedBy', { provider: 'ComicVine' })}
         </div>
     );
 }
