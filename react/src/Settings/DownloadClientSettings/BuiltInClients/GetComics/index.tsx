aa208d31735d7ecb882774aa04b852e8543602da
matt1432 <matt@nelim.org>
2025-09-09 18:38:34 -0400

    feat(react): implement GetComics and Libgen+ modals

--- react/src/Settings/DownloadClientSettings/BuiltInClients/GetComics/index.tsx
+++ react/src/Settings/DownloadClientSettings/BuiltInClients/GetComics/index.tsx
@@ -1,20 +1,54 @@
-// TODO:
 // IMPORTS
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Redux
+import { useGetSettingsQuery, useSaveSettingsMutation } from 'Store/Api/Settings';
 
 // Misc
 import translate from 'Utilities/String/translate';
 
 // General Components
 import Button from 'Components/Link/Button';
+import FieldSet from 'Components/FieldSet';
+import FormGroup from 'Components/Form/FormGroup';
+import FormInputGroup from 'Components/Form/FormInputGroup';
+import FormLabel from 'Components/Form/FormLabel';
 import ModalBody from 'Components/Modal/ModalBody';
 import ModalFooter from 'Components/Modal/ModalFooter';
 
 // Specific Components
 import BuiltInClient from '../BuiltInClient';
 
+// Types
+import type { CheckInputChanged } from 'typings/Inputs';
+
 // IMPLEMENTATIONS
 
 export default function GetComics() {
+    const [saveSettings] = useSaveSettingsMutation();
+
+    const { enableGetcomics } = useGetSettingsQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            enableGetcomics: Boolean(data?.enableGetcomics),
+        }),
+    });
+
+    const [enable, setEnable] = useState(Boolean(enableGetcomics));
+
+    useEffect(() => {
+        setEnable(Boolean(enableGetcomics));
+    }, [enableGetcomics]);
+
+    const handleEnableChange = useCallback(
+        ({ value }: CheckInputChanged<'enable'>) => {
+            setEnable(value);
+            saveSettings({ enableGetcomics: value });
+        },
+        [saveSettings],
+    );
+
     return (
         <BuiltInClient title="GetComics">
             {(onModalClose) => (
@@ -40,6 +74,18 @@ export default function GetComics() {
                             downloads speed goes down a lot. Preferring external services in the
                             service preference list is advised in order to avoid this.
                         </p>
+
+                        <FieldSet legend={translate('Settings')}>
+                            <FormGroup>
+                                <FormLabel>{translate('Enable')}</FormLabel>
+                                <FormInputGroup
+                                    type="check"
+                                    name="enable"
+                                    onChange={handleEnableChange}
+                                    value={enable}
+                                />
+                            </FormGroup>
+                        </FieldSet>
                     </ModalBody>
 
                     <ModalFooter>
--- react/src/Settings/DownloadClientSettings/BuiltInClients/LibgenPlus/index.tsx
+++ react/src/Settings/DownloadClientSettings/BuiltInClients/LibgenPlus/index.tsx
@@ -1,20 +1,54 @@
-// TODO:
 // IMPORTS
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Redux
+import { useGetSettingsQuery, useSaveSettingsMutation } from 'Store/Api/Settings';
 
 // Misc
 import translate from 'Utilities/String/translate';
 
 // General Components
 import Button from 'Components/Link/Button';
+import FieldSet from 'Components/FieldSet';
+import FormGroup from 'Components/Form/FormGroup';
+import FormInputGroup from 'Components/Form/FormInputGroup';
+import FormLabel from 'Components/Form/FormLabel';
 import ModalBody from 'Components/Modal/ModalBody';
 import ModalFooter from 'Components/Modal/ModalFooter';
 
 // Specific Components
 import BuiltInClient from '../BuiltInClient';
 
+// Types
+import type { CheckInputChanged } from 'typings/Inputs';
+
 // IMPLEMENTATIONS
 
 export default function LibgenPlus() {
+    const [saveSettings] = useSaveSettingsMutation();
+
+    const { enableLibgen } = useGetSettingsQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            enableLibgen: Boolean(data?.enableLibgen),
+        }),
+    });
+
+    const [enable, setEnable] = useState(Boolean(enableLibgen));
+
+    useEffect(() => {
+        setEnable(Boolean(enableLibgen));
+    }, [enableLibgen]);
+
+    const handleEnableChange = useCallback(
+        ({ value }: CheckInputChanged<'enable'>) => {
+            setEnable(value);
+            saveSettings({ enableLibgen: value });
+        },
+        [saveSettings],
+    );
+
     return (
         <BuiltInClient title="Libgen+">
             {(onModalClose) => (
@@ -28,6 +62,18 @@ export default function LibgenPlus() {
                             can search this website to find downloads for the volumes in your
                             library.
                         </p>
+
+                        <FieldSet legend={translate('Settings')}>
+                            <FormGroup>
+                                <FormLabel>{translate('Enable')}</FormLabel>
+                                <FormInputGroup
+                                    type="check"
+                                    name="enable"
+                                    onChange={handleEnableChange}
+                                    value={enable}
+                                />
+                            </FormGroup>
+                        </FieldSet>
                     </ModalBody>
 
                     <ModalFooter>
