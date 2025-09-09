43f0024119d960f088cceb06c6d04f124827fccf
matt1432 <matt@nelim.org>
2025-09-09 14:45:23 -0400

    feat(react): add test all download clients button logic

--- react/src/Settings/DownloadClientSettings/index.tsx
+++ react/src/Settings/DownloadClientSettings/index.tsx
@@ -2,8 +2,13 @@
 // IMPORTS
 
 // React
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 // Redux
+import {
+    useGetDownloadClientsQuery,
+    useTestDownloadClientMutation,
+} from 'Store/Api/DownloadClients';
 
 // Misc
 import { icons } from 'Helpers/Props';
@@ -23,12 +28,36 @@ import DownloadClients from './DownloadClients';
 // CSS
 
 // Types
+import type { DownloadClient } from 'typings/DownloadClient';
 
 // IMPLEMENTATIONS
 
 export default function MediaManagement() {
-    const isTestingAll = false;
-    const handleTestAllIndexersPress = () => {};
+    const { clients, hasNoClients } = useGetDownloadClientsQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            clients: data ?? [],
+            hasNoClients: (data?.length ?? 0) === 0,
+        }),
+    });
+
+    const [testDownloadClient] = useTestDownloadClientMutation();
+
+    const [toTest, setToTest] = useState<DownloadClient[]>([]);
+    const isTestingAll = useMemo(() => toTest.length !== 0, [toTest.length]);
+
+    const handleTestAllIndexersPress = useCallback(() => {
+        setToTest(clients);
+    }, [clients]);
+
+    useEffect(() => {
+        if (toTest.length === 0) {
+            return;
+        }
+
+        testDownloadClient(toTest[0]).finally(() => {
+            setToTest(toTest.slice(1));
+        });
+    }, [testDownloadClient, toTest]);
 
     return (
         <PageContent title={translate('DownloadClientSettings')}>
@@ -40,12 +69,13 @@ export default function MediaManagement() {
                         iconName={icons.TEST}
                         isSpinning={isTestingAll}
                         onPress={handleTestAllIndexersPress}
+                        isDisabled={hasNoClients || isTestingAll}
                     />
                 }
             />
 
             <PageContentBody>
-                <FieldSet legend={translate('BuiltInClients')}></FieldSet>
+                <FieldSet legend={translate('BuiltInClients')}>TODO:</FieldSet>
 
                 <FieldSet legend={translate('TorrentClients')}>
                     <DownloadClients />
