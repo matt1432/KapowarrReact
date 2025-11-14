69c6e6964713f40ae3b20c47914dd8d7cc992664
matt1432 <matt@nelim.org>
2025-11-14 01:28:35 -0500

    chore: update lockfiles

--- frontend/default.nix
+++ frontend/default.nix
@@ -9,7 +9,7 @@ buildNpmPackage {
 
   src = ./.;
 
-  npmDepsHash = "sha256-TxBWorlLHC1U4oPzxNYCf1sFXmRSbQrUriwLcRr/f5Y=";
+  npmDepsHash = "sha256-Aw1y8G8wltQO359PO7+ecBt7/vi9B0gaX3cGIPmUw+Y=";
 
   installPhase = ''
     runHook preInstall
